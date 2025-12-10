36e6120b06d2f877eebcc90762b4d9d971248d7f
Maria Eduarda <ferreirabatistam34@gmail.com>
2025-12-10 20:54:24 -0300

    feat: adding routes for plano de saude and consulta

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 const { Router } = require("express")
 const PacienteController = require("./controllers/PacienteController")
 const MedicoController = require("./controllers/MedicoController")
+const PlanoSaudeController = require("./controllers/PlanoSaudeController")
+const ConsultaController = require("./controllers/ConsultaController")
+const MedicoPlanoController = require("./controllers/MedicoPlanoController")
 
 const routes = Router()
 
@@ -19,5 +22,21 @@ routes.get("/medicos", MedicoController.index)
 routes.get("/medicos/:id", MedicoController.show)
 routes.put("/medicos/:id", MedicoController.update)
 routes.delete("/medicos/:id", MedicoController.destroy)
+
+routes.post("/planos", PlanoSaudeController.store)
+routes.get("/planos", PlanoSaudeController.index)
+routes.get("/planos/:id", PlanoSaudeController.show)
+routes.put("/planos/:id", PlanoSaudeController.update)
+routes.delete("/planos/:id", PlanoSaudeController.destroy)
+
+routes.post("/consultas", ConsultaController.store)
+routes.get("/consultas", ConsultaController.index)
+routes.get("/consultas/:id", ConsultaController.show)
+routes.put("/consultas/:id", ConsultaController.update)
+routes.delete("/consultas/:id", ConsultaController.destroy)
+
+routes.post("/medicos-planos", MedicoPlanoController.store)
+routes.get("/medicos/:medico_id/planos", MedicoPlanoController.index)
+routes.delete("/medicos/:medico_id/planos/:plano_id", MedicoPlanoController.destroy)
 
 module.exports = routes
